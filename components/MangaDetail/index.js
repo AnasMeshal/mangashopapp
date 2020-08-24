0e7643036e89af44a156737ce87410b6e5785b17
AnasMeshal <Fire-Ball-KwT@hotmail.com>
2020-08-24 23:09:49 +0300

    created toast

--- components/MangaDetail/index.js
+++ components/MangaDetail/index.js
@@ -11,7 +11,7 @@ import {
   MangaAuthor,
   StyledButton,
 } from "./styles";
-import { Body, Text } from "native-base";
+import { Body, Text, Toast, Root } from "native-base";
 import cartStore from "../../stores/cartStore";
 
 const MangaDetail = ({ route }) => {
@@ -22,37 +22,51 @@ const MangaDetail = ({ route }) => {
   const handleAdd = () => {
     const newItem = { qty, mangaId: manga.id };
     cartStore.addItemToCart(newItem);
+    Toast.show({
+      text: `${qty} ${
+        qty === 1 ? manga.name : manga.name + "s"
+      } Successfully Added to your Cart!`,
+      textStyle: { color: "black" },
+      duration: 3000,
+      style: {
+        backgroundColor: "white",
+        textAlign: "center",
+      },
+    });
+    setQty(1);
   };
 
   return (
-    <BackgorundImage source={require(`../../solidBlack.jpg`)}>
-      <MangaImage
-        source={
-          manga.image
-            ? {
-                uri: manga.image,
-              }
-            : require(`../../mangacover.jpg`)
-        }
-      />
-      <MangaName>{manga.name}</MangaName>
-      <MangaDescription>{manga.description}</MangaDescription>
-      <MangaPrice>{manga.price} Kd</MangaPrice>
-      <MangaAuthor>author: {manga.author}</MangaAuthor>
-      <Body>
-        <NumericInput
-          textColor="#fff"
-          initValue={1}
-          rounded
-          minValue="1"
-          onChange={setQty}
-          value={qty}
+    <Root>
+      <BackgorundImage source={require(`../../solidBlack.jpg`)}>
+        <MangaImage
+          source={
+            manga.image
+              ? {
+                  uri: manga.image,
+                }
+              : require(`../../mangacover.jpg`)
+          }
         />
-        <StyledButton onPress={handleAdd}>
-          <Text>Add</Text>
-        </StyledButton>
-      </Body>
-    </BackgorundImage>
+        <MangaName>{manga.name}</MangaName>
+        <MangaDescription>{manga.description}</MangaDescription>
+        <MangaPrice>{manga.price} Kd</MangaPrice>
+        <MangaAuthor>author: {manga.author}</MangaAuthor>
+        <Body>
+          <NumericInput
+            textColor="#fff"
+            initValue={1}
+            rounded
+            minValue="1"
+            onChange={setQty}
+            value={qty}
+          />
+          <StyledButton onPress={handleAdd}>
+            <Text>Add</Text>
+          </StyledButton>
+        </Body>
+      </BackgorundImage>
+    </Root>
   );
 };
 
